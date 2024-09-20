293a59453fedecd51adc7d76e4126776b035745c
timurgepard <timur-ishuov@rambler.ru>
2024-09-20 19:47:59 +0200

    Update train.py

--- train.py
+++ train.py
@@ -34,8 +34,8 @@ start_episode = 1 #number for the identification of the current episode
 episode_rewards_all, episode_steps_all, test_rewards, Q_learning = [], [], [], False
 
 
-capacity = 256000
-batch_lim = 768
+capacity = 1024*1000
+batch_lim = 1024
 fade_factor = 10 # fading memory factor, flat region before gradual forgeting
 tau = 0.005
 
@@ -244,6 +244,9 @@ if not Q_learning:
     log_file.write("experiment_started\n")
     total_steps = 0
 
+    algo.actor.apply(init_weights)
+    algo.critic.apply(init_weights)
+
 
     while not Q_learning:
         rewards = []
@@ -258,6 +261,11 @@ if not Q_learning:
             np.random.seed(r2)
             random.seed(r3)
 
+            #------------------learning will not depend on initial weights------------------------
+            algo.actor.apply(add_weights)
+            algo.critic.apply(add_weights)
+          
+
             if total_steps>=explore_time and not Q_learning: Q_learning = True
             action = max_action.numpy()*np.random.uniform(-0.5, 1.0, size=action_dim)
             next_state, reward, done, truncated, info = env.step(action)
@@ -270,6 +278,10 @@ if not Q_learning:
         Return = np.sum(rewards)
         print(f" Rtrn = {Return:.2f}")
 
+
+    algo.actor.apply(scale_weights)
+    algo.critic.apply(scale_weights)
+    algo.critic_target.load_state_dict(algo.critic.state_dict())
 
     total_steps = 0
     print("copying explore data")
