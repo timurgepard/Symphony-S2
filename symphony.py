8e3080d12142adb00462c7d320d78608da8df142
timurgepard <timur-ishuov@rambler.ru>
2024-12-05 02:00:17 +0500

    Update symphony.py

--- symphony.py
+++ symphony.py
@@ -339,7 +339,7 @@ class ReplayBuffer:
     def __init__(self, state_dim, action_dim, device, capacity):
 
         self.capacity, self.length, self.device = capacity, 0, device
-        self.batch_size = 64 + min(self.length//500, 512) #in order for sample to describe population
+        self.batch_size = 64 + min(self.length//333, 704) #in order for sample to describe population
         self.random = np.random.default_rng()
         self.indices, self.indexes, self.probs = [], np.array([]), np.array([])
         self.ratio = 0.0
@@ -368,7 +368,7 @@ class ReplayBuffer:
             self.indices.append(self.length-1)
             self.indexes = np.array(self.indices)
             self.probs = self.fade(self.indexes/self.length) if self.length>1 else np.array([0.0])
-            self.batch_size = 64 + min(self.length//500, 512)
+            self.batch_size = 64 + min(self.length//333, 704)
             self.ratio = self.length/self.capacity
             
             
