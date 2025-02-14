0cf19b30ac3918acf6d5c569c252a60afb968259
timurgepard <timur-ishuov@rambler.ru>
2025-02-14 19:57:15 +0500

    Update symphony.py

--- symphony.py
+++ symphony.py
@@ -184,14 +184,12 @@ class ActorCritic(jit.ScriptModule):
 
 
     #========= Actor Forward Pass =========
-
     @jit.script_method
-    def actor(self, state):
+    def actor(self, state, noise:bool=True):
         x = self.a(state).clamp(-3.0, 3.0).reshape(-1,2,self.action_dim)
         x_lim = self.a_max*torch.sigmoid(2*x[:,0]/self.a_max)
-        x = x[:,1] + 0.05 * self.a_max * torch.randn_like(x[:,1]).clamp(-3.0, 3.0)
+        if noise: x = x[:,1] + 0.1 * self.a_max * torch.randn_like(x[:,1]).clamp(-3.0, 3.0)
         return x_lim*torch.tanh(x/x_lim), x_lim
-
 
 
     #========= Critic Forward Pass =========
