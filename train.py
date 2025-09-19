6f123b433df8aaa88510536b3781b7b03c3c2c6a
timurgepard <timur-ishuov@rambler.ru>
2025-09-19 12:59:21 +0500

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,9 @@ import gymnasium as gym
 import random
 import pickle
 from symphony import Symphony, log_file
+import time
 
+np.set_printoptions(threshold=10000, linewidth=200)
 
 def seed_reset():
     r1, r2, r3 = random.randint(0,2**32-1), random.randint(0,2**32-1), random.randint(0,2**32-1)
@@ -27,7 +29,7 @@ def seed_reset():
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 learning_rate = 3e-4
-explore_time = 5120
+explore_time = 10240
 limit_step = 1000 #max steps per episode
 limit_eval = 1000 #max steps per evaluation
 num_episodes = 1000000
@@ -56,7 +58,6 @@ elif option == 4:
     env_test = gym.make('Ant-v4')
 
 elif option == 5:
-    burst = True
     env = gym.make('Swimmer-v4')
     env_test = gym.make('Swimmer-v4')
 
@@ -64,10 +65,13 @@ elif option == 6:
     env = gym.make('Hopper-v4')
     env_test = gym.make('Hopper-v4')
 
+elif option == 7:
+    env = gym.make('Pusher-v4')
+    env_test = gym.make('Pusher-v4')
 
 state_dim = env.observation_space.shape[0]
 action_dim= env.action_space.shape[0]
-#max_action = torch.FloatTensor(env.action_space.high) if env.action_space.is_bounded() else 1.0
+#max_action = torch.FloatTensor(env.action_space.high) if env.action_space.is_bounded() else torch.ones(action_dim)
 max_action = torch.ones(action_dim)
 
 print("action_dim: ", action_dim, "state_dim: ", state_dim, "max_action:", max_action)
@@ -95,8 +99,8 @@ def testing(env, limit_step, test_episodes, current_step=0, save_log=False):
         rewards = []
 
         for steps in range(1,limit_step+1):
-            seed_reset()
-            action = algo.select_action(state)
+            #seed_reset()
+            action, scale = algo.select_action(state, noise=True)
             next_state, reward, done, truncated, info = env.step(action)
             rewards.append(reward)
             state = next_state
@@ -124,21 +128,8 @@ def save_data_models():
     torch.save(algo.nets.target.state_dict(), 'nets_target_model.pt')
     torch.save(algo.nets_optimizer.state_dict(), 'nets_optimizer.pt')
     with open('data', 'wb') as file:
-        pickle.dump({'buffer': algo.replay_buffer, 'q_next_ema': algo.nets.q_next_ema, 'episode_rewards_all':episode_rewards_all, 'episode_steps_all':episode_steps_all, 'total_steps': total_steps}, file)
+        pickle.dump({'buffer': algo.replay_buffer, 'q_next_ema': algo.nets.q_next_ema, 'eta': algo.nets.eta, 'episode_rewards_all':episode_rewards_all, 'episode_steps_all':episode_steps_all, 'total_steps': total_steps}, file)
     print("...saved")
-
-def hard_recovery(algo, replay_buffer, size):
-    algo.replay_buffer.states[:size] = replay_buffer.states[:size]
-    algo.replay_buffer.actions[:size] = replay_buffer.actions[:size]
-    algo.replay_buffer.rewards[:size] = replay_buffer.rewards[:size]
-    algo.replay_buffer.next_states[:size] = replay_buffer.next_states[:size]
-    algo.replay_buffer.not_dones_gamma[:size] = replay_buffer.not_dones_gamma[:size]
-    algo.replay_buffer.eta = replay_buffer.eta
-    algo.replay_buffer.indices = replay_buffer.indices[:size]
-    algo.replay_buffer.indexes = replay_buffer.indexes[:size]
-    algo.replay_buffer.length = replay_buffer.length
-    algo.replay_buffer.probs = replay_buffer.probs[:size]
-    print("hard_recovery... eta: ", algo.replay_buffer.eta, ", buffer length:", algo.replay_buffer.length)
 
 
 try:
@@ -146,8 +137,8 @@ try:
     with open('data', 'rb') as file:
         dict = pickle.load(file)
         algo.replay_buffer = dict['buffer']
-        #hard_recovery(algo, dict['buffer'], 735998) # comment the previous line and chose a memory size to recover from old buffer
-        algo.q_next_ema = dict['q_next_ema']
+        algo.nets.eta = dict['eta']
+        algo.nets.q_next_ema = dict['q_next_ema']
         episode_rewards_all = dict['episode_rewards_all']
         episode_steps_all = dict['episode_steps_all']
         total_steps = dict['total_steps']
@@ -194,7 +185,7 @@ if not Q_learning:
         for steps in range(1, limit_step+1):
 
             seed_reset()
-            action = algo.select_action(state, explore=True)
+            action, scale = algo.select_action(state)
             next_state, reward, done, truncated, info = env_test.step(action)
             rewards.append(reward)
             if algo.replay_buffer.length>=explore_time and not Q_learning: Q_learning = True; break
@@ -205,6 +196,7 @@ if not Q_learning:
         Return = np.sum(rewards)
         print(f" Rtrn = {Return:.2f}")
 
+    
 
 #==============================================================================================
 #==============================================================================================
@@ -212,10 +204,7 @@ if not Q_learning:
 #==============================================================================================
 #==============================================================================================
 
-
-
-
-
+algo.norm_fill()
 
 for i in range(start_episode, num_episodes):
 
@@ -224,7 +213,6 @@ for i in range(start_episode, num_episodes):
    
 
     for steps in range(1, limit_step+1):
-
         scale = algo.train()
         seed_reset()
 
@@ -233,21 +221,19 @@ for i in range(start_episode, num_episodes):
         # save models, data
         if (total_steps>=2500 and total_steps%2500==0):
             testing(env_test, limit_step=limit_eval, test_episodes=25, current_step=total_steps, save_log=True)
-            if total_steps%5000==0: save_data_models()
+            if total_steps%2500==0: save_data_models()
 
 
-        action = algo.select_action(state)
+        action, _ = algo.select_action(state)
         next_state, reward, done, truncated, info = env.step(action)
         rewards.append(reward)
         algo.replay_buffer.add(state, action, reward, next_state, done)
         if done: break
         state = next_state
 
+    
     episode_rewards_all.append(np.sum(rewards))
     episode_steps_all.append(steps)
 
-    #print(f"Ep {i}: Rtrn = {episode_rewards_all[-1]:.2f} | ep steps = {steps} | total_steps = {total_steps} ")
-    #log_file.write_opt(str(i) + " : " + str(round(episode_rewards_all[-1], 2)) + " : step : " + str(total_steps) + "\n")
-
-    print(f"Ep {i}: Rtrn = {episode_rewards_all[-1]:.2f} | ep steps = {steps} | total_steps = {total_steps}  | scale = {scale:.4f}  ")
-    log_file.write_opt(str(i) + " : " + str(round(episode_rewards_all[-1], 2)) + " : step : " + str(total_steps) + " : scale : " + str(round(scale, 4))  + "\n")
+    print(f"Ep {i}: Rtrn = {episode_rewards_all[-1]:.2f} | ep steps = {steps} | total_steps = {total_steps}  | scale = {scale.mean().item():.4f}  ")
+    log_file.write_opt(str(i) + " : " + str(round(episode_rewards_all[-1], 2)) + " : step : " + str(total_steps) + " : S : " + str(round(scale.mean().item(), 4)) + " : S list :" + str(np.round(scale.mean(dim=0).cpu().numpy(), decimals=3))  + "\n")
