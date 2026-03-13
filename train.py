c0d5a25cc15f97d931aa9557223482934968f612
timurgepard <timur-ishuov@rambler.ru>
2026-03-13 09:21:28 +0500

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ torch.cuda.empty_cache()
 print(device)
 G = 3 # update-to-data ratio
 learning_rate = 1e-4
-explore_time, times = 20480, 25
+explore_time, times = 25600, 30
 capacity = explore_time * times
 h_dim = 512
 num_episodes = 1000000
@@ -33,7 +33,7 @@ episode_rewards, episode_steps, Q_learning, total_steps = [], [], False, 0
 env_name = 'Humanoid-v4'
 
 
-pre_valid = False # testing models when loaded
+pre_valid = True # testing models when loaded
 env = gym.make(env_name)
 env_test = gym.make(env_name)
 env_valid = gym.make(env_name, render_mode="human")
@@ -120,9 +120,10 @@ def save(algo, episode_return, episode_steps, total_steps):
     torch.save(algo.nets.online.state_dict(), 'nets_online_model.pt')
     torch.save(algo.nets.target.state_dict(), 'nets_target_model.pt')
     torch.save(algo.nets_optimizer.state_dict(), 'nets_optimizer.pt')
-    print("saving... the buffer length = ", algo.replay_buffer.length, " avg return = ", average_return, " avg steps = ", average_steps, end="")
+    print("saving... the buffer length = ", algo.nets.replay_buffer.length.item(), " avg return = ", average_return, " avg steps = ", average_steps, end="")
+    torch.save(algo.nets.replay_buffer.state_dict(), 'replay_buffer.pt')
     with open('data', 'wb') as file:
-        pickle.dump({'buffer': algo.replay_buffer, 'q_next_ema': algo.nets.q_next_ema, 'episode_return': episode_return, 'episode_steps': episode_steps, 'total_steps' : total_steps}, file)
+        pickle.dump({'q_next_ema': algo.nets.q_next_ema, 'episode_return': episode_return, 'episode_steps': episode_steps, 'total_steps' : total_steps}, file)
     print(" > done")
 
 
@@ -143,14 +144,14 @@ def load(algo, Q_learning):
 
     try:
         print("loading buffer...")
+        algo.nets.replay_buffer.load_state_dict(torch.load('replay_buffer.pt'))
         with open('data', 'rb') as file:
             dict = pickle.load(file)
-            algo.replay_buffer = dict['buffer']
             algo.nets.q_next_ema = dict['q_next_ema']
             episode_return = dict['episode_return']
             episode_steps = dict['episode_steps']
             total_steps = dict['total_steps']
-            if algo.replay_buffer.length>=explore_time and not Q_learning: Q_learning = True
+            if algo.nets.replay_buffer.length>=explore_time and not Q_learning: Q_learning = True
         
         print('buffer loaded, Q_ema', round(algo.nets.q_next_ema.item(), 2), ', average_reward = ', round(np.mean(episode_return[-300:]), 2))
         
@@ -188,9 +189,9 @@ def sim_loop(env, episodes, testing, Q_learning, algo, episode_return, episode_s
             if testing:
                 Q_learning = False
             else:
-                if algo.replay_buffer.length>=explore_time and not Q_learning:
+                if algo.nets.replay_buffer.length>=explore_time and not Q_learning:
                     Q_learning = True
-                    algo.replay_buffer.norm_fill(times)
+                    algo.nets.replay_buffer.norm_fill(times)
                     print("started training")
                     save(algo, episode_return, episode_steps, total_steps)
 
@@ -205,14 +206,14 @@ def sim_loop(env, episodes, testing, Q_learning, algo, episode_return, episode_s
 
 
             # if steps is close to episode limit (e.g. 900) we shut down actions and leave noise to get Terminal Transition:
-            active = steps<min(average_steps+150, limit_step-150) if Q_learning else True
+            active = steps<(limit_step-100) if Q_learning else True
             action = algo.select_action(state,  active=active, noise=not testing)
             next_state, reward, done, truncated, info = env.step(action)
-            if not testing: algo.replay_buffer.add(state, action, reward, next_state, done)
+            if not testing: algo.nets.replay_buffer.add(state, action, reward, next_state, done)
             Return += reward
             
             # actual training
-            if Q_learning: [algo.train() for _ in range(G)]
+            if Q_learning: [algo.update() for _ in range(G)]
             if done or truncated: break
             state = next_state
 
@@ -234,7 +235,7 @@ def sim_loop(env, episodes, testing, Q_learning, algo, episode_return, episode_s
 
 # Loading existing models
 Q_learning, episode_return, episode_steps, total_steps = load(algo, Q_learning)
-if not Q_learning: log_file.clean(); algo.replay_buffer.init()
+if not Q_learning: log_file.clean(); algo.nets.replay_buffer.init()
 
 # Training
 sim_loop(env, num_episodes, False, Q_learning, algo, episode_return, episode_steps, total_steps)
