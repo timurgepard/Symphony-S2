c6e3b07e74b310223a2e9f80210ba8c10c917a42
timurgepard <timur-ishuov@rambler.ru>
2026-03-30 08:57:34 +0500

    Update train.py

--- train.py
+++ train.py
@@ -19,9 +19,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
 
 print(device)
-G = 3
-learning_rate = 5e-5
-explore_time, times = 20480, 25
+learning_rate = 1e-4
+explore_time, times = 25600, 30
 capacity = explore_time * times
 h_dim = 512
 num_episodes = 1000000
@@ -33,7 +32,7 @@ episode_rewards, episode_steps, Q_learning, total_steps = [], [], False, 0
 env_name = 'Humanoid-v4'
 
 
-pre_valid = False # testing models when loaded
+pre_valid = True # testing models when loaded
 env = gym.make(env_name)
 env_test = gym.make(env_name)
 env_valid = gym.make(env_name, render_mode="human")
@@ -198,7 +197,7 @@ def sim_loop(env, episodes, testing, Q_learning, algo, episode_return, episode_s
 
             # if total steps is divisible to 2500 save models, stop training and do testing, return to training:
             if Q_learning and total_steps>=2500 and total_steps%2500==0:
-                if total_steps%10000==0: save(algo, episode_return, episode_steps, total_steps)
+                if total_steps%25000==0: save(algo, episode_return, episode_steps, total_steps)
                 
                 print("start testing")
                 test_return = sim_loop(env_test, 25, True, Q_learning, algo, [], [], total_steps=0)
@@ -214,7 +213,7 @@ def sim_loop(env, episodes, testing, Q_learning, algo, episode_return, episode_s
             Return += reward
             
             # actual training
-            if Q_learning: [algo.update() for _ in range(G)]
+            if Q_learning: algo.update()
             if done or truncated: break
             state = next_state
 
